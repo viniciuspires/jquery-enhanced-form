699382090e0aaf928bce726b1c7191234152aa35
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2013-07-10 23:56:42 -0300

    Adjusting some options and demonstrating the possible and tested functionalities

---
+++ demo/echo.php
@@ -0,0 +1,6 @@
+<?php
+
+var_dump( array(
+	'method' => $_SERVER['REQUEST_METHOD'],
+	'data' => $_POST
+));
--- jquery.enhancedform.js
+++ jquery.enhancedform.js
@@ -1,74 +1,51 @@
-(function($){
+;(function($){
     $.fn.enhancedform = function(options) {
-
         return this.each(function(){
             var $this = $(this);
 
             var settings = $.extend({
-                waiting:function() {
-                    $this.find('button[type=submit]')
-                        .html('Salvando...').addClass('disabled');
-                },
-                done:function(data) {
-                    console.log(data);
-
-                    var alertClassMap = {
-                        ok:'success',
-                        error:'alert',
-                        'undefined':''
-                    };
-
-                    MessageBroker.sendMessage(alertClassMap[data.tipo], data.mensagem);
-                },
-                error:function() {
-
-                },
-                after:function(jqXhr, textStatus){
-                    $this.find('button[type=submit]')
-                        .removeClass('disabled')
-                        .html('Salvar');
-                    console.log(jqXhr, textStatus);
-                },
-                statusMap:{
-                    404:function(){
-                        MessageBroker.sendMessage( 'alert', 'Página do servidor não encontrada.' );
-                    },
-                    500:function(){
-                        MessageBroker.sendMessage( 'alert', 'Ocorreu um erro interno no servidor. Entre em contato com o administrador.');
-                    }
+                waiting:function() {},
+                done:function(data) {},
+                error:function() {},
+                after:function(jqXhr, textStatus) {},
+                status: {
+                    // Examples
+                    403:function(){},
+                    404:function(){},
+                    500:function(){}
                 }
-            }, options);
+            }, $.extend( $this.data(), options) );
 
             $this.submit(function(e){
                 e.preventDefault();
 
-                var action  = $(this).attr('action');
-                var method  = $(this).attr('method');
-                var accepts = $(this).data('accepts');
+                var $this = $(this);
 
-                var waitingFunction = settings.waiting;
+                var action  = $this.attr('action');
+                var method  = $this.attr('method');
+                var accepts = $this.data('accepts');
+
+                var waiting = settings.waiting;
                 if ( $this.data('waiting') != undefined ) {
-                    var waitingFunction = eval($(this).data('waiting'));
+                    waiting = eval($this.data('waiting'));
                 }
 
                 var callback = settings.done;
                 if ( $this.data('done') != undefined ) {
-                    var callback = eval( $(this).data('done') );
+                    callback = eval( $this.data('done') );
                 }
                 
-                var dataHashMap = $this.serialize();
-
-                console.log("Sending to '"+action+"' via "+method.toUpperCase()+":", dataHashMap);
+                var formData = $this.serialize();
 
                 $.ajax({
                     url:action,
                     type:method,
                     dataType:accepts,
-                    data:dataHashMap,
+                    data:formData,
                     success:callback,
-                    beforeSend:waitingFunction,
+                    beforeSend:waiting,
                     complete:settings.after,
-                    statusCode:settings.statusMap
+                    statusCode:settings.status
                 });
 
             });
@@ -93,7 +70,7 @@
                     $this.data('autosaveId', newId);
                 };
 
-                $this.change(callback).keyup(callback);
+                $this.on('change keyup', callback);
             }            
         });
     };
