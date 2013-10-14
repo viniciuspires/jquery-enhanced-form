d00b7cc0c8628a89bbb0d02b278f089d017e8148
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2013-10-14 11:06:08 -0300

    Removing demo files, adding more comments, and adding a lib folder

--- demo/echo.php
+++
@@ -1,4 +0,0 @@
-<?php var_dump( array(
-	'method' => $_SERVER['REQUEST_METHOD'],
-	'data' => $_POST
-));
--- jquery.enhancedform.js
+++ jquery.enhancedform.js
@@ -3,25 +3,34 @@
         return this.each(function(){
             var $this = $(this);
 
+            // Javascript arguments override data attributes that overrides default settings
             var settings = $.extend({
+                // A function that executes while the form is submitting (expecting a function name)
                 waiting:function(){},
+                // Executes when obtained success in the request
                 done:function(data) {},
+                // Function for error handling
                 error:function() {},
+                // Function to execute everytime a request is finished
                 after:function(jqXhr, textStatus) {},
+                // The HTTP header "Accepts", defaults to json
                 accepts:'json',
+                // A JS Object mapping status codes
                 status: {
-                    // Examples
+                    // Status code examples
                     403:function(){},
                     404:function(){},
                     500:function(){}
                 }
             }, $.extend( $this.data(), options) );
 
+            // Binds an ajax submit handler
             $this.submit(function(e){
                 e.preventDefault();
                 var $this = $(this);
 
                 if ( typeof settings.done == 'string' ) {
+                    // Eval is evil, I know.
                     settings.done = eval(settings.done);
                 }
 
@@ -29,11 +38,15 @@
                     settings.waiting = eval(settings.waiting);
                 }
                 
+                // @todo Should be an object instead of a query string
                 var formData = $this.serialize();
 
                 $.ajax({
+                    // Gets the standard "action" attribute to submit
                     url:        $this.attr('action'),
+                    // Same thing with the "method" attribute, could be any HTTP method
                     type:       $this.attr('method'),
+                    // HTTP "Accepts" header
                     dataType:   settings.accepts,
                     data:       formData,
                     success:    settings.done,
@@ -41,10 +54,10 @@
                     complete:   settings.after,
                     statusCode: settings.status
                 });
-
             });
 
-            if ( $this.data('autosave') != undefined ) {
+            // Checks if it is an Autosaved form
+            if ( $this.attr('data-autosave') != undefined ) {
                 $this.on('change keyup', function(e){
                     var id = $this.data('autosaveId');
 
